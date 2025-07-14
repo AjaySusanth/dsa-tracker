c83d11b7b9d3931bbfdf4b9a17915cd3f644486e
AjaySusanth <ajaysusanth10@gmail.com>
2025-07-14 17:27:10 +0530

    minor changes for ci

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,14 +5,16 @@ import { PrismaClient } from './generated/prisma'
 const PORT = process.env.PORT || 4000
 const prisma = new PrismaClient()
 
-app.listen(PORT,()=>{
-    console.log(`Server running on ${PORT}`)    
-})
+
 async function connectDB() {
     try {
 
         await prisma.$connect()
         console.log("Connected to Postgres")
+
+        app.listen(PORT,()=>{
+            console.log(`Server running on ${PORT}`)    
+        })
     
     } catch (error) {
         console.error('❌ Error starting server:', error);
